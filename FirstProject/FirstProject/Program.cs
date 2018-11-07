619f502233a729cc431729c02d944409ff956deb
Alexandr Vyroubal <xvyroub@fi.muni.cz>
2018-11-07 18:58:01 +0100

    Hello, World!

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstProject
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // comment 
+            Console.WriteLine("Hello, World! - I can code in C#!");
+            Console.ReadKey();
+        }
+    }
+}
